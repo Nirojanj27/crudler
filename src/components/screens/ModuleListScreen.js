e7b4c31fab7e7cbac0334b8c6cd1fe011a0c06b6
Nirojanj27 <Nirojan@myyahoo.com>
2025-11-22 22:36:02 +0000

    Completed changes 2

---
+++ src/components/screens/ModuleListScreen.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, } from 'react-native'
+import Screen from './layout/Screen';
+
+const ModuleListScreen = () => {
+  //Initiallsations __________________
+  //State  ________________________
+  //Handlers ______________________
+  //View _____________________________
+  return (
+    <Screen>
+      <Text>List</Text>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+
+export default ModuleListScreen;
---
+++ src/components/screens/ModuleViewScreen.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, } from 'react-native'
+import Screen from './layout/Screen';
+
+const ModuleViewScreen = () => {
+  //Initiallsations __________________
+  //State  ________________________
+  //Handlers ______________________
+  //View _____________________________
+  return (
+    <Screen>
+      <Text>View</Text>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+
+export default ModuleViewScreen;
