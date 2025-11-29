b1e9cfbb779e1890a958f85f7972b6d50ddf307b
Nirojanj27 <Nirojan@myyahoo.com>
2025-11-29 19:54:19 +0000

    Completed changes 5 (#4)

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { StyleSheet} from 'react-native'
 import Screen from './layout/Screen';
 import ModuleList from '../entity/modules/ModulesList.js';
@@ -6,15 +7,16 @@ import initialModules from '../../data/modules.js';
 
 const ModuleListScreen = () => {
   //Initiallsations __________________
-  const modules = initialModules;
-
   //State  ________________________
+  const [modules, setModules] = useState(initialModules);
+
   //Handlers ______________________
-  const handleSelect = (module) => alert(`Item ${module.ModuleCode} Selected`);
+  const handleDelete = (module) =>  setModules( modules.filter( (item) => item.ModuleID !== module.ModuleID) );
+  
   //View _____________________________
   return (
     <Screen>
-      <ModuleList modules={modules} onSelect={handleSelect} />
+      <ModuleList modules={modules} onSelect={handleDelete} />
     </Screen>
   );
 }
